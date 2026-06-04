c1840c8aea79a5e4a54e0b914cb67a23f1f9c65c
Gerasim Troeglazov <3dEyes@gmail.com>
2026-06-04 21:41:33 +1000

    Thread throttling

--- src/tracer/utils/ParallelUtils.h
+++ src/tracer/utils/ParallelUtils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025, Gerasim Troeglazov, 3dEyes@gmail.com. All rights reserved.
+ * Copyright 2025-2026, Gerasim Troeglazov, 3dEyes@gmail.com. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
@@ -14,40 +14,55 @@
 
 class ParallelUtils {
 public:
-	static void ParallelFor(int start, int end, std::function<void(int)> func) {
-		int totalIterations = end - start;
-		if (totalIterations <= 0) return;
+    static void ParallelFor(int start, int end, std::function<void(int)> func) {
+	int totalIterations = end - start;
+	if (totalIterations <= 0) return;
 
-		unsigned int numThreads = std::thread::hardware_concurrency();
-		if (numThreads == 0) numThreads = 2;
-		
-		if ((unsigned int)totalIterations < numThreads) 
-			numThreads = totalIterations;
+	const int minIterationsPerThread = 32;
 
-		std::vector<std::future<void> > futures;
-		int blockSize = totalIterations / numThreads;
-		int remainder = totalIterations % numThreads;
+	unsigned int hardwareThreads = std::thread::hardware_concurrency();
+	if (hardwareThreads == 0) hardwareThreads = 2;
 
-		int currentStart = start;
-
-		for (unsigned int i = 0; i < numThreads; ++i) {
-			int currentEnd = currentStart + blockSize + (i < (unsigned int)remainder ? 1 : 0);
-			
-			if (currentEnd > currentStart) {
-				futures.push_back(std::async(std::launch::async, [currentStart, currentEnd, func]() {
-					for (int j = currentStart; j < currentEnd; ++j) {
-						func(j);
-					}
-				}));
-			}
-			
-			currentStart = currentEnd;
-		}
-
-		for (size_t i = 0; i < futures.size(); i++) {
-			futures[i].wait();
-		}
+	unsigned int numThreads = totalIterations / minIterationsPerThread;
+	if (numThreads > hardwareThreads) {
+	    numThreads = hardwareThreads;
 	}
+	if (numThreads == 0) {
+	    numThreads = 1;
+	}
+
+	// If workload is too small, execute sequentially on the caller thread
+	if (numThreads <= 1) {
+	    for (int j = start; j < end; ++j) {
+		func(j);
+	    }
+	    return;
+	}
+
+	std::vector<std::future<void> > futures;
+	int blockSize = totalIterations / numThreads;
+	int remainder = totalIterations % numThreads;
+
+	int currentStart = start;
+
+	for (unsigned int i = 0; i < numThreads; ++i) {
+	    int currentEnd = currentStart + blockSize + (i < (unsigned int)remainder ? 1 : 0);
+
+	    if (currentEnd > currentStart) {
+		futures.push_back(std::async(std::launch::async, [currentStart, currentEnd, func]() {
+		    for (int j = currentStart; j < currentEnd; ++j) {
+			func(j);
+		    }
+		}));
+	    }
+
+	    currentStart = currentEnd;
+	}
+
+	for (size_t i = 0; i < futures.size(); i++) {
+	    futures[i].wait();
+	}
+    }
 };
 
 #endif
